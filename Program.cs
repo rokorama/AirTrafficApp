491e4bb821dcef731f1e031e46836c0022b2837e
Rokas <rokamanauskas@gmail.com>
2022-09-26 13:35:04 +0300

    Add .gitignore, rename RadarService to something more accurate

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ internal class Program
     public static void Main(string[] args)
     {
         var host = Host.CreateDefaultBuilder(args)
-                       .ConfigureServices(services => { services.AddTransient<RadarService>(); })
+                       .ConfigureServices(services => { services.AddTransient<AirTrafficControlService>(); })
                        .Build();
 
-        var radar = host.Services.GetRequiredService<RadarService>();
+        var radar = host.Services.GetRequiredService<AirTrafficControlService>();
         radar.StartApplication();
         radar.ProcessInput();
     }
